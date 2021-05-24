fb864948c4bd318862af88e44ac6b163b25ca01f
lomoo <sulaimanmarouf@gmail.com>
2021-05-24 12:25:58 -0700

    switch to yarn

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,19 +5,9 @@ module.exports = {
     node: true,
     es6: true,
   },
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: module,
-    allowImportExportEverywhere: true,
-    parserOptions: {
-     project: ['./tsconfig.json'],
-    },
-    ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-    }
-  }, // to enable features such as async/await
+  parserOptions: { ecmaVersion: 11 }, // to enable features such as async/await
   ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
-  extends: ['eslint:recommended', 'airbnb-typescript'],
+  extends: ['eslint:recommended'],
   overrides: [
     // This configuration will apply only to TypeScript files
     {
@@ -30,20 +20,17 @@ module.exports = {
         es6: true,
       },
       extends: [
-        'eslint:recommended',
         'airbnb-typescript',
-        'plugin:import/typescript',
+        'eslint:recommended',
         'plugin:@typescript-eslint/recommended', // TypeScript rules
         'plugin:react/recommended', // React rules
         'plugin:react-hooks/recommended', // React hooks rules
         'plugin:jsx-a11y/recommended', // Accessibility rules
-        'plugin:prettier/recommended', // Prettier plugin
+        'plugin:prettier/recommended',
       ],
       rules: {
         // We will use TypeScript's types for component props instead
         'react/prop-types': 'off',
-
-        'react/jsx-filename-extension': [1, { "extensions": [".tsx", ".ts"] }],
 
         // No need to import React when using Next.js
         'react/react-in-jsx-scope': 'off',
@@ -54,8 +41,6 @@ module.exports = {
         // Why would you want unused vars?
         '@typescript-eslint/no-unused-vars': ['error'],
 
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
-        
         // I suggest this setting for requiring return types on functions only where useful
         '@typescript-eslint/explicit-function-return-type': [
           'warn',
@@ -64,6 +49,7 @@ module.exports = {
             allowConciseArrowFunctionExpressionsStartingWithVoid: true,
           },
         ],
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
       },
     },
   ],
